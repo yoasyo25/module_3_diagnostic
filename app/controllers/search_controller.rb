6b8753e85e7d336cd3a793d1763500c1f27694a5
Yohanan Assefa <nanahoy@gmail.com>
2017-08-30 09:36:13 -0600

    Adds index view for search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,20 @@
 class SearchController < ApplicationController
   def index
+    search = params[:q]
+
+    @conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json") do |faraday|
+      faraday.headers["API-Key"] = ENV["api_key"]
+      faraday.adapter  Faraday.default_adapter
+    end
+
+    response = @conn.get("fuel_type=&zip=#{search}&limit=6")
   end
 
 end
+
+#
+# https://developer.nrel.gov/api/alt-fuel-stations/v1.json?
+# fuel_type=E85,ELEC
+# &state=CA&
+# limit=2&
+# api_key=3rnpPSszHWLRwYU2eWj7tVhgCF59MPvCNJeMrUrH&format=JSON
